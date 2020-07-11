a841b27fc9de430505c1385986525d36318f73e8
serhat toy <ugurserhattoy@gmail.com>
2020-07-11 23:34:08 +0300

    v.0.0

---
+++ app/src/main/java/com/ust/weatherforecastapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.ust.weatherforecastapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var  navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        navController = findNavController(R.id.fragment)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
+    }
+}
---
+++ app/src/main/java/com/ust/weatherforecastapp/register/RegisterViewModel.kt
@@ -0,0 +1,7 @@
+package com.ust.weatherforecastapp.register
+
+import androidx.lifecycle.ViewModel
+
+class RegisterViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
